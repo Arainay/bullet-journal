f61718ca77f5519db6a811bbd585e9988698a35f
arainay <arainay@gmail.com>
2020-05-12 16:22:13 +0800

    Added constants

---
+++ src/helpers/constants.js
@@ -0,0 +1,14 @@
+export const TIME_PERIOD = {
+  YEAR: 'year',
+  MONTH: 'month',
+  WEEK: 'week',
+  DAY: 'day'
+};
+
+export const ROUTES = {
+  TASKS: '/tasks',
+  IDEAS: '/ideas',
+  NOTES: '/notes',
+  CALENDAR: '/calendar',
+  RESULT: '/result'
+};

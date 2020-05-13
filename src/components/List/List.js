e14d182712193c4d51c90b5c1d231e8d95ceb191
arainay <arainay@gmail.com>
2020-05-13 14:28:08 +0800

    Added create and search

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import Form, { Input } from '@app/components/Form';
+import Svg from '@app/components/Svg';
+import createIcon from './assets/create.svg';
 import './list.scss';
 
 const List = ({ type = 'tasks' }) => {
+  const { pathname } = useLocation();
+  const { push } = useHistory();
+
+  const toCreate = () => {
+    push(`${pathname}/create`);
+  };
+
+  const search = event => {
+    console.log(event.target['search'].value);
+  };
+
   return (
     <article className="list">
       <h1 className="list__header">{type}</h1>
-      <Form className="list__search">
-        <Input type="search" className="list__search-input"/>
+      <Form className="list__search" onSubmit={search}>
+        <Input
+          type="search"
+          className="list__search-input"
+          placeholder="Search"
+          name="search"
+        />
       </Form>
 
       <footer className="list__control">
-
+        <Svg className="list__create" content={createIcon} onClick={toCreate}/>
       </footer>
     </article>
   );

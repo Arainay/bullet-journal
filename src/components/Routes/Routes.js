1a54a508e42a6f226e1f6c9a340d3902e560607b
arainay <arainay@gmail.com>
2020-05-13 13:52:50 +0800

    Added tasks and ideas routes

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,16 +2,17 @@ import React, { lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { ROUTES } from '@app/helpers/constants';
 
+const List = lazy(() => import(/* webpackPrefetch: true */ '@app/components/List'));
 const ResultsChart = lazy(() => import(/* webpackPrefetch: true */ '@app/components/ResultsChart'));
 
 const Routes = () => (
   <main className="main">
     <Switch>
       <Route path={ROUTES.TASKS}>
-
+        <List/>
       </Route>
       <Route path={ROUTES.IDEAS}>
-
+        <List type="Ideas"/>
       </Route>
       <Route path={ROUTES.CALENDAR}>
 

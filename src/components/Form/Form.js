2cd3f8cf6e87806e242e7b56f01c8da78116a85f
arainay <arainay@gmail.com>
2020-05-13 14:26:34 +0800

    Added preventDefault for the form submit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,11 +2,16 @@ import React from 'react';
 import classnames from '@app/helpers/classnames';
 
 const Form = ({ className, children, onSubmit, ...props }) => {
+  const submit = event => {
+    event.preventDefault();
+    onSubmit(event);
+  };
+
   return (
     <form
       {...props}
       className={classnames('form', className)}
-      onSubmit={onSubmit}
+      onSubmit={submit}
     >
       {children}
     </form>

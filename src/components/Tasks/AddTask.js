3b5d8c15a9f434e7b6c1c6e445e9578597fcf0ef
arainay <arainay@gmail.com>
2020-05-14 14:54:58 +0800

    Added form and creation handler

--- src/components/Tasks/AddTask.js
+++ src/components/Tasks/AddTask.js
@@ -1,20 +1,69 @@
 import React, { useContext } from 'react';
+import { Button, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
 import { TasksContext } from '@app/providers/Tasks';
 import Form, { Input } from '@app/components/Form';
 
 const AddTask = () => {
-  // eslint-disable-next-line no-unused-vars
   const { addTask } = useContext(TasksContext);
 
   const add = event => {
-    const [name] = event.target;
+    const task = {
+      name: event.target['name'].value,
+      important: event.target['important'].checked,
+      startDate: Date.parse(event.target['beginningDate'].value),
+      endDate: Date.parse(event.target['finishTo'].value),
+      createdAt: Date.now()
+    };
 
-    console.log(name.value);
+    addTask(task);
   };
 
   return (
     <Form className="add-task" onSubmit={add}>
-      <Input className="add-task__input add-task__name" name="name"/>
+      <fieldset className="add-task__fieldset add-task__header">
+        <Input className="add-task__name" name="name"/>
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="important"
+              color="primary"
+            />
+          }
+          label="Important"
+        />
+      </fieldset>
+      <fieldset className="add-task__fieldset add-task__description">
+        <textarea className="add-task__description"/>
+      </fieldset>
+      <fieldset className="add-task__fieldset add-task__date">
+        <TextField
+          type="date"
+          defaultValue="2017-05-24"
+          className="add-task__date-picker"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          label="Beginning date"
+          name="beginningDate"
+          id="beginningDate"
+        />
+        <TextField
+          type="date"
+          defaultValue="2017-05-25"
+          className="add-task__date-picker"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          label="Finish to"
+          name="finishTo"
+          id="finishTo"
+        />
+      </fieldset>
+      <Button type="submit" variant="contained" color="primary">
+        Create
+      </Button>
     </Form>
   );
 };
